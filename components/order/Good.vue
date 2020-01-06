af58627e43d50a13cff61f350e69c386ac303bde
xiaolipotter <371478779@qq.com>
2020-01-06 11:45:02 +0800

    refund

--- components/order/Good.vue
+++ components/order/Good.vue
@@ -26,7 +26,7 @@
 					<view class="fll">
 						价格:¥{{item.price || '0'}}元/{{item.goodsUnit || '斤'}}
 					</view>
-					<view class="refund flr" v-if="item.isAfterSale == 0" @click.stop="goRefund">退款</view>
+					<view class="refund flr" v-if="item.isAfterSale == 0" @click.stop="applyRefund(item)">退款</view>
 				</view>
 			</view>
 		</view>
@@ -70,8 +70,10 @@
 		},
 		methods: {
 			// 去申请退款页面
-			goRefund(){
-				
+			applyRefund(item){
+				uni.navigateTo({
+				  url: '/pages/refund/apply?orderId=' + item.orderId
+				})
 			},
 			// 去商品详细页面
 			goGoodsDetail() {
---
+++ pages/refund/apply.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="apply-refund">
+		
+	</view>
+</template>
+
+<script>
+  import {loadRefundInfo} from '@/api/refund.js'
+	var vm =  {
+		data() {
+      vm = this;
+			return {
+				
+			};
+		},
+    onLoad(options){
+      loadRefundInfo({
+        orderDetailId: options.orderId
+      }).then(data=>{
+        
+      })
+    }
+	}
+  export default vm
+</script>
+
+<style lang="scss" scoped>
+.apply-refund{
+  
+}
+</style>
