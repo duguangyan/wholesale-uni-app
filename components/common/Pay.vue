89cca8ffbab1ab93e686e7bff5b0b6566e1691c9
xiaolipotter <371478779@qq.com>
2020-01-11 17:58:15 +0800

    fail pay to detail

--- components/common/Pay.vue
+++ components/common/Pay.vue
@@ -132,13 +132,13 @@ var vm = {
                     if (data.err_msg == 'get_brand_wcpay_request:ok') {
                       // 使用以上方式判断前端返回,微信团队郑重提示：
                       //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
-                      uni.redirectTo({
-                        url: `/pages/user/order/success?order=${self.orderId}&shopId=${self.shopId}`
+                      return uni.redirectTo({
+                        url: `/pages/user/order/success?orderId=${self.orderId}&shopId=${self.shopId}`
                       })
                     }
-                    // vm.$loading.close();
-                    // let { orderId, shopId } = this.$route.query;
-                    // this.getOrderDetailById(orderId, shopId);
+                    uni.redirectTo({
+                      url: `/pages/user/order/detail?orderId=${self.orderId}&shopId=${self.shopId}`
+                    })
                   }
                 );
               }
