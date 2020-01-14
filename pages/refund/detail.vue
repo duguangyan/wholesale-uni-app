69d9fc0df9310130bf07baa5692a4fdfc3058c83
xiaolipotter <371478779@qq.com>
2020-01-14 11:19:10 +0800

    fix iden in after detail

--- pages/refund/detail.vue
+++ pages/refund/detail.vue
@@ -62,12 +62,14 @@
       <view>退款说明：{{ detail.afterSaleDetail.descs }}</view>
       <view>退款单号：{{ detail.afterSaleDetail.afterSaleId }}</view>
 
-      <view class="upload-iden">凭证</view>
-      <view class="iden">
-        <view class="item" v-if="detail.afterSaleDetail.afterSaleImgs" v-for="(item, index) in detail.afterSaleDetail.afterSaleImgs" :key="index" @click="showBigImg(item.url)">
-          <image class="upload-img" :src="item.url" mode=""></image>
+      <template v-if="detail.afterSaleDetail.afterSaleImgs && detail.afterSaleDetail.afterSaleImgs.length>0">
+        <view class="upload-iden">凭证</view>
+        <view class="iden">
+          <view class="item" v-for="(item, index) in detail.afterSaleDetail.afterSaleImgs" :key="index" @click="showBigImg(item.url)">
+            <image class="upload-img" :src="item.url" mode=""></image>
+          </view>
         </view>
-      </view>
+      </template>
     </view>
 
     <view v-if="isDialog" class="dialog">
