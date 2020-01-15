408a5cf7eb7879579f1d59e3d053816cadc1c439
duguangyan <dis2010@126.com>
2020-01-15 14:26:41 +0800

    发货

--- pages/user/order/detail.vue
+++ pages/user/order/detail.vue
@@ -477,13 +477,13 @@
 			// 发货
 			deliverGoods(){
 				let n = 0
-				this.order.shopOrder.orderDetailList.forEach(item=>{
-					if(item.isAfterSale == 1){
-						n++
-						T.tips("该订单有部分商品退款未完成，无法进行发货")
-						return false
-					}
-				})
+				// this.order.shopOrder.orderDetailList.forEach(item=>{
+				// 	if(item.isAfterSale == 1){
+				// 		n++
+				// 		T.tips("该订单有部分商品退款未完成，无法进行发货")
+				// 		return false
+				// 	}
+				// })
 				if(n == 0){
 					if(this.order.shopOrder.sendType == 1){
 						uni.navigateTo({
